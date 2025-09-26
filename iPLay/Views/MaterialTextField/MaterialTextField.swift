76acd7d4c1ddf6c2d0a924dec9871da212608f6c
Javier Pineda <jpgpineda98@gmail.com>
2025-09-25 23:13:58 -0600

    adding custom textfield

--- iPLay/Views/MaterialTextField/MaterialTextField.swift
+++ iPLay/Views/MaterialTextField/MaterialTextField.swift
@@ -9,78 +9,67 @@ import UIKit
 
 @IBDesignable
 class FloatingLabeledTextField: UITextField {
+    var floatingLabel: UILabel = UILabel(frame: CGRect.zero) // Label
+    var floatingLabelHeight: CGFloat = 14 // Default height
     
-    var floatingLabel: UILabel!
-    var placeHolderText: String?
-    
-    @IBInspectable var floatingLabelColor: UIColor = UIColor.blue {
+    @IBInspectable
+    var _placeholder: String? // we cannot override 'placeholder'
+    @IBInspectable
+    var floatingLabelColor: UIColor = UIColor.black {
         didSet {
             self.floatingLabel.textColor = floatingLabelColor
+            self.setNeedsDisplay()
         }
     }
-    
-    @IBInspectable var floatingLabelFont: UIFont = UIFont.systemFont(ofSize: 15) {
+    @IBInspectable
+    var activeBorderColor: UIColor = UIColor.blue
+    @IBInspectable
+    var floatingLabelFont: UIFont = UIFont.systemFont(ofSize: 14) {
         didSet {
-            self.floatingLabel.font = floatingLabelFont
+            self.floatingLabel.font = self.floatingLabelFont
+            self.font = self.floatingLabelFont
+            self.setNeedsDisplay()
         }
-    }
-    
-    @IBInspectable var errorBorderColor: UIColor = .red {
-        didSet {
-            self.layer.borderColor = errorBorderColor.cgColor
-        }
-    }
-    
-    var floatingLabelHeight: CGFloat = 30
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
     }
     
     required init?(coder aDecoder: NSCoder) {
-        
         super.init(coder: aDecoder)
-        
-        let flotingLabelFrame = CGRect(x: 0, y: 0, width: frame.width, height: 0)
-        
-        floatingLabel = UILabel(frame: flotingLabelFrame)
-        floatingLabel.textColor = floatingLabelColor
-        floatingLabel.font = floatingLabelFont
-        floatingLabel.text = self.placeholder
-        
-        self.addSubview(floatingLabel)
-        placeHolderText = placeholder
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(textFieldDidBeginEditing), name: UITextField.textDidBeginEditingNotification, object: self)
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(textFieldDidEndEditing), name: UITextField.textDidEndEditingNotification, object: self)
-        
-        
+        self._placeholder = (self._placeholder != nil) ? self._placeholder : placeholder // Use our custom placeholder if none is set
+        placeholder = self._placeholder // make sure the placeholder is shown
+        self.floatingLabel = UILabel(frame: CGRect.zero)
+        self.addTarget(self, action: #selector(self.addFloatingLabel), for: .editingDidBegin)
+        self.addTarget(self, action: #selector(self.removeFloatingLabel), for: .editingDidEnd)
     }
     
-    @objc func textFieldDidBeginEditing(_ textField: UITextField) {
-        
+    // Add a floating label to the view on becoming first responder
+    @objc func addFloatingLabel() {
         if self.text == "" {
-            UIView.animate(withDuration: 0.3) {
-                self.floatingLabel.frame = CGRect(x: 0, y: -self.floatingLabelHeight, width: self.frame.width, height: self.floatingLabelHeight)
-            }
+            self.floatingLabel.textColor = floatingLabelColor
+            self.floatingLabel.font = floatingLabelFont
+            self.floatingLabel.text = self._placeholder
+            self.floatingLabel.layer.backgroundColor = UIColor.white.cgColor
+            self.floatingLabel.translatesAutoresizingMaskIntoConstraints = false
+            self.floatingLabel.clipsToBounds = true
+            self.floatingLabel.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.floatingLabelHeight)
+    self.layer.borderColor = self.activeBorderColor.cgColor
+            self.addSubview(self.floatingLabel)
+          
+            self.floatingLabel.bottomAnchor.constraint(equalTo:
+            self.topAnchor, constant: -10).isActive = true // Place our label 10pts above the text field
+            // Remove the placeholder
             self.placeholder = ""
         }
+        self.setNeedsDisplay()
     }
     
-    @objc func textFieldDidEndEditing(_ textField: UITextField) {
-        
+    @objc func removeFloatingLabel() {
         if self.text == "" {
-            UIView.animate(withDuration: 0.1) {
-                self.floatingLabel.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: 0)
+            UIView.animate(withDuration: 0.13) {
+               self.subviews.forEach{ $0.removeFromSuperview() }
+               self.setNeedsDisplay()
             }
-            self.placeholder = placeHolderText
+            self.placeholder = self._placeholder
         }
-    }
-    
-    deinit {
-        
-        NotificationCenter.default.removeObserver(self)
-        
+        self.layer.borderColor = UIColor.black.cgColor
     }
 }
